10b60bff3fb0ee6af09bc5194f5e72e7b38e483f
Marta Kruk <kruk.marta@outlook.com>
2022-04-02 15:11:51 +0200

    first commit

---
+++ src/main/java/com/collectibles/front/views/home/HomeView.java
@@ -0,0 +1,32 @@
+package com.collectibles.front.views.home;
+
+import com.collectibles.front.views.MainLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Home")
+@Route(value = "home", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class HomeView extends VerticalLayout {
+
+    public HomeView() {
+        setSpacing(false);
+
+        add(new H1("Welcome to Collectibles!"));
+        add(new H3("Here you can store your favourite books and quotes and browse an open library"));
+
+        add(new H2("\"Random quote\""));
+        add(new Paragraph("- Author"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+}
---
+++ src/main/java/com/collectibles/front/views/search/SearchView.java
@@ -0,0 +1,134 @@
+package com.collectibles.front.views.search;
+
+import com.collectibles.front.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import java.util.Arrays;
+import java.util.List;
+
+@PageTitle("Search")
+@Route(value = "search", layout = MainLayout.class)
+public class SearchView extends Div implements AfterNavigationObserver {
+
+    private TextField searchTextField = new TextField();
+    private Button searchButton;
+    private HorizontalLayout searchLayout = new HorizontalLayout();
+    Grid<Person> grid = new Grid<>();
+
+    public SearchView() {
+        searchTextField.setPlaceholder("Search by title");
+        searchTextField.setClearButtonVisible(true);
+
+        searchButton = new Button("Search");
+        searchButton.addClickListener(e -> {
+            update();
+        });
+
+        searchLayout.setPadding(true);
+        searchLayout.add(searchTextField);
+        searchLayout.add(searchButton);
+
+        addClassName("search-view");
+        setSizeFull();
+        grid.setHeight("100%");
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
+        grid.addComponentColumn(person -> createCard(person));
+        add(searchLayout, grid);
+    }
+
+    private HorizontalLayout createCard(Person person) {
+        HorizontalLayout card = new HorizontalLayout();
+        card.addClassName("card");
+        card.setSpacing(false);
+        card.getThemeList().add("spacing-s");
+
+        Image image = new Image();
+        image.setSrc(person.getImage());
+        VerticalLayout description = new VerticalLayout();
+        description.addClassName("description");
+        description.setSpacing(false);
+        description.setPadding(false);
+
+        HorizontalLayout header = new HorizontalLayout();
+        header.addClassName("header");
+        header.setSpacing(false);
+        header.getThemeList().add("spacing-s");
+
+        Span name = new Span(person.getName());
+        name.addClassName("name");
+        Span date = new Span(person.getDate());
+        date.addClassName("date");
+        header.add(name, date);
+
+        Span post = new Span(person.getPost());
+        post.addClassName("post");
+
+        HorizontalLayout actions = new HorizontalLayout();
+        actions.addClassName("actions");
+        actions.setSpacing(false);
+        actions.getThemeList().add("spacing-s");
+
+        Icon likeIcon = VaadinIcon.HEART.create();
+        likeIcon.addClassName("icon");
+        Span likes = new Span(person.getLikes());
+        likes.addClassName("likes");
+        Icon commentIcon = VaadinIcon.COMMENT.create();
+        commentIcon.addClassName("icon");
+        Span comments = new Span(person.getComments());
+        comments.addClassName("comments");
+        Icon shareIcon = VaadinIcon.CONNECT.create();
+        shareIcon.addClassName("icon");
+        Span shares = new Span(person.getShares());
+        shares.addClassName("shares");
+
+        actions.add(likeIcon, likes, commentIcon, comments, shareIcon, shares);
+
+        description.add(header, post, actions);
+        card.add(image, description);
+        return card;
+    }
+
+    @Override
+    public void afterNavigation(AfterNavigationEvent event) {
+        update();
+    }
+
+    private static Person createPerson(String image, String name, String date, String post, String likes,
+            String comments, String shares) {
+        Person p = new Person();
+        p.setImage(image);
+        p.setName(name);
+        p.setDate(date);
+        p.setPost(post);
+        p.setLikes(likes);
+        p.setComments(comments);
+        p.setShares(shares);
+
+        return p;
+    }
+
+    private void update() {
+        // Set some data when this view is displayed.
+        List<Person> persons = Arrays.asList( //
+                createPerson("https://randomuser.me/api/portraits/men/42.jpg", "John Smith", "May 8",
+                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
+                        "1K", "500", "20")
+        );
+
+        grid.setItems(persons);
+    }
+
+}
